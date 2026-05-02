2cfffc4eb939d204ae072113603c174dedbde510
Luis Villa <luis.villa@111btec.com>
2026-05-02 12:46:09 -0600

    (Update) Se ajusto la estructura de código base para detectar cuando una RFID esta sobre el lector (reproducir cancion) y detectar cuando se quita (detener cancion).

--- src/src.ino
+++ src/src.ino
@@ -13,32 +13,32 @@ HardwareSerial dfSerial(2); // UART2
 DFRobotDFPlayerMini dfPlayer;
 
 // ---- Variables globales ----
+String uidActual = "";
 bool tarjetaPresente = false;
 
-unsigned long ultimaLectura = 0;
-const unsigned long TIMEOUT = 1500;
+unsigned long ultimoPing = 0;
+unsigned long ultimaRespuesta = 0;
 
-byte ultimoUID[10];
-byte ultimoSize = 0;
+const unsigned long intervaloPing = 100;
+const unsigned long timeoutRFID = 500;
 
 // Estructura de las TAGs
 struct RFIDTag {
-  byte uid[7];
-  byte length;
+  String uid;
   int track;
 };
 
 // BD
 RFIDTag tags[] = {
-  {{0x53, 0x80, 0x7D, 0x12, 0x13, 0x00, 0x01}, 7, 1},
-  {{0x53, 0x7F, 0x7D, 0x12, 0x13, 0x00, 0x01}, 7, 2},
-  {{0x53, 0x7E, 0x7D, 0x12, 0x13, 0x00, 0x01}, 7, 3},
+  {"53807D12130001", 1},
+  {"537F7D12130001", 2},
+  {"537E7D12130001", 3},
 };
 
 // ---- Prototipos de funciones ----
-int buscarTrack(byte *uidLeido, byte longitud);
-bool hayTarjetaPresente();
-void detectarNuevaTarjeta();
+int buscarTrack(String uidActual);
+bool hayTarjetaPresente(); // TODO
+void detectarNuevaTarjeta(); // TODO
 
 void setup() {
   Serial.begin(115200);
@@ -54,60 +54,101 @@ void setup() {
 
   if (dfPlayer.begin(dfSerial)) {
     Serial.println("DFPlayer OK");
-    dfPlayer.volume(20);
+    dfPlayer.volume(15);
   } else {
     Serial.println("Error DFPlayer");
   }
 }
 
 void loop() {
+  detectarNuevaTarjeta();
 
-  if (!rfid.PICC_IsNewCardPresent()) {
-    tarjetaPresente = false;
-    return;
+  if (tarjetaPresente){
+    verificarPresenciaRFID();
   }
+}
 
-  if (tarjetaPresente) return;
+void detectarNuevaTarjeta() {
+  if (!rfid.PICC_IsNewCardPresent()) return;
 
   if (!rfid.PICC_ReadCardSerial()) return;
 
-  tarjetaPresente = true;
+  String nuevoUID = obtenerUID();
 
-  int track = buscarTrack(rfid.uid.uidByte, rfid.uid.size);
+  if (nuevoUID != uidActual) {
+    uidActual = nuevoUID;
+    tarjetaPresente = true;
+
+    reproducirCancion(uidActual);
+  }
+
+  ultimaRespuesta = millis();
+
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
+}
+
+void verificarPresenciaRFID() {
+  if (millis() - ultimoPing < intervaloPing) return;
+
+  ultimoPing = millis();
+
+  byte bufferATQA[2];
+  byte bufferSize = sizeof(bufferATQA);
+
+  MFRC522::StatusCode status = rfid.PICC_WakeupA(bufferATQA, &bufferSize);
+
+  if (status == MFRC522::STATUS_OK) {
+    ultimaRespuesta = millis();
+  }
+
+  if (millis() - ultimaRespuesta > timeoutRFID) {
+    dfPlayer.stop();
+
+    tarjetaPresente = false;
+    uidActual = "";
+  }
+}
+
+String obtenerUID() {
+  String uid = "";
+
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    if (rfid.uid.uidByte[i] < 0x10) {
+      uid += "0"; // Si el dato es entre 0 y 9 agregamos un 0 para mantener el formato correcto.
+    }
+
+    uid += String(rfid.uid.uidByte[i], HEX);
+  }
+
+  uid.toUpperCase();
+  return uid;
+}
+
+void reproducirCancion(String uidActual) {
+  int track = buscarTrack(uidActual);
 
   if (track != -1) {
     Serial.print("Reproduciendo track: ");
     Serial.println(track);
 
-    dfPlayer.play(track); // 🔥 AQUÍ YA FUNCIONA TODO
+    dfPlayer.play(track);
   } else {
-    Serial.println("UID no reconocido");
+    Serial.print("UID no reconocido: ");
+    Serial.println(uidActual);
   }
-
-  rfid.PICC_HaltA();
 }
 
-// ---- Función para comparar tags validas ----
-int buscarTrack(byte *uidLeido, byte longitud) {
+int buscarTrack(String uidActual) {
+  uidActual.toUpperCase();
+
   int numTags = sizeof(tags) / sizeof(tags[0]);
 
   for (int i = 0; i < numTags; i++) {
-
-    if (tags[i].length != longitud) continue;
-
-    bool match = true;
-
-    for (byte j = 0; j < longitud; j++) {
-      if (uidLeido[j] != tags[i].uid[j]) {
-        match = false;
-        break;
-      }
-    }
-
-    if (match) {
+    if (uidActual == tags[i].uid) {
       return tags[i].track;
     }
   }
 
-  return -1; // no encontrado
+  return -1; // No encontrado
 }
